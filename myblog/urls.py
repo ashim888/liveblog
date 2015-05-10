f19400634b337e5d89386f0ddc0b260e56055644
Ashim Lamichhane <punchedrock@gmail.com>
2015-05-10 12:35:24 +0545

    Pushing the whole temp codes to the git::FIRST COMMIT::

---
+++ myblog/urls.py
@@ -0,0 +1,21 @@
+__author__ = 'ashim888'
+from django.conf.urls import include, url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.post_list),
+    url(r'^index', views.post_list),
+    url(r'^post_list', views.post_list),
+    url(r'^about',views.about_me),
+    url(r'^contact',views.contact_me),
+    url(r'^test',views.test),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
+]
+
+# from django.conf.urls import include, url
+# from . import views
+#
+# urlpatterns = [
+#     url(r'^$', views.post_list),
+#     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
+# ]
---
+++ static/js/app.js
@@ -0,0 +1,4 @@
+/**
+ * Created by ashim888 on 4/23/15.
+ */
+var app=angular.module("BoltNetworkApp",[]);
